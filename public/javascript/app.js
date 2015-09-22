9699ef5c9a427732239255f65332327cae9b58f3
Rubina Pradhan <pradhanr03@gmail.com>
2015-09-22 12:38:44 -0400

    cleaning code

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,87 +1,102 @@
 $(document).ready(function() {
-  $('[data-toggle="tooltip"]').tooltip();
-  $('#send').click(mail);
+    $('[data-toggle="tooltip"]').tooltip();
+    $('#send').click(mail);
+
 
 
 });
 
 $(window).bind('scroll', function(e) {
-  parallaxScroll();
+    parallaxScroll();
 
 });
 
 
 function parallaxScroll() {
-  var scrolledY = $(window).scrollTop();
+    var scrolledY = $(window).scrollTop();
 
-  $('#pic1').css('top', '-' + ((scrolledY * 0.41)) + 'px');
+    $('#pic1').css('top', '-' + ((scrolledY * 0.41)) + 'px');
 
+}
+
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR has 'withCredentials' property only if it supports CORS
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") { // if IE use XDR
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
 }
 
 
 
 
 var mail = function() {
-  console.log('hello');
-  var popup;
-  var data = {
-    mail: $('[name=mail]').val(),
-    message: $('[name=message]').val()
-  }
-  var email = $('[name=mail]').val();
-
-
-  if ((data.mail.length < 1) || (data.message.length < 1) || (data.mail.length < 1 && data.message.length < 1)) {
-
-    $('#popup').html('Please enter a valid email or message  ' + '<i class="fa fa-smile-o"></i>');
-    $("#popup").delay(4000).fadeOut("slow", function() {
-      $(this).remove();
-    });
-    popup = $('<h3>').attr('id', 'popup');
-    $('.footer-icons').append(popup);
-  } else {
-    var revString = email.split('').reverse().join('');
-    var rev = revString.slice(4, 9);
-    var string = rev.split('').reverse().join('');
-
-    if (string === 'yahoo') {
-      $('#popup').html('Please enter another email besides a yahoo email  ' + '<i class="fa fa-smile-o"></i>');
-      $("#popup").delay(4000).fadeOut("slow", function() {
-        $(this).remove();
-      });
-      popup = $('<h3>').attr('id', 'popup');
-      $('.footer-icons').append(popup);
-
-      $('[name=mail]').val('');
-      $('[name=message]').val('');
-    } else {
-      $('#popup').html('Thank you for the email ' + '<i class="fa fa-smile-o"></i>');
-      $("#popup").delay(4000).fadeOut("slow", function() {
-        $(this).remove();
-      });
-
-      popup = $('<h3>').attr('id', 'popup');
-      $('.footer-icons').append(popup);
-
-
-
-      $('[name=mail]').val('');
-      $('[name=message]').val('');
-
-
-
-      $.ajax({
-        type: "POST",
-        url: '/send',
-        data: data,
-        success: function() {
-
-        },
-        fail: function() {
-
-        }
-      });
+    console.log('hello');
+    var popup;
+    var data = {
+        mail: $('[name=mail]').val(),
+        message: $('[name=message]').val()
     }
-  }
+    var email = $('[name=mail]').val();
+
+
+    if ((data.mail.length < 1) || (data.message.length < 1) || (data.mail.length < 1 && data.message.length < 1)) {
+
+        $('#popup').html('Please enter a valid email or message  ' + '<i class="fa fa-smile-o"></i>');
+        $("#popup").delay(4000).fadeOut("slow", function() {
+            $(this).remove();
+        });
+        popup = $('<h3>').attr('id', 'popup');
+        $('.footer-icons').append(popup);
+    } else {
+        var revString = email.split('').reverse().join('');
+        var rev = revString.slice(4, 9);
+        var string = rev.split('').reverse().join('');
+
+        if (string === 'yahoo') {
+            $('#popup').html('Please enter another email besides a yahoo email  ' + '<i class="fa fa-smile-o"></i>');
+            $("#popup").delay(4000).fadeOut("slow", function() {
+                $(this).remove();
+            });
+            popup = $('<h3>').attr('id', 'popup');
+            $('.footer-icons').append(popup);
+
+            $('[name=mail]').val('');
+            $('[name=message]').val('');
+        } else {
+            $('#popup').html('Thank you for the email ' + '<i class="fa fa-smile-o"></i>');
+            $("#popup").delay(4000).fadeOut("slow", function() {
+                $(this).remove();
+            });
+
+            popup = $('<h3>').attr('id', 'popup');
+            $('.footer-icons').append(popup);
+
+
+
+            $('[name=mail]').val('');
+            $('[name=message]').val('');
+
+
+
+            $.ajax({
+                type: "POST",
+                url: '/send',
+                data: data,
+                success: function() {
+
+                },
+                fail: function() {
+
+                }
+            });
+        }
+    }
 
 };
--- server.js
+++ server.js
@@ -25,12 +25,12 @@ app.set('port', (process.env.PORT || 3000));
 
 
 app.listen(app.get('port'), function() {
-  console.log("App running on port : ", app.get('port'));
+    console.log("App running on port : ", app.get('port'));
 });
 
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  extname: 'handlebars'
+    defaultLayout: 'main',
+    extname: 'handlebars'
 }));
 app.set('views', path.join(root, 'views'));
 app.set('view engine', 'handlebars');
@@ -39,23 +39,20 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 app.use(bodyParser.json());
 
 
 
 app.use(methodOverride(function(req, res) {
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
-  }
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
 }));
-
-
-
 
 
 
@@ -71,29 +68,29 @@ app.use(methodOverride(function(req, res) {
 
 
 app.post('/send', function(req, res) {
-  var mail = req.body.mail;
-  var message = req.body.message;
+    var mail = req.body.mail;
+    var message = req.body.message;
 
-  var revString = mail.split('').reverse().join('');
-  var rev = revString.slice(4, 9);
-  var string = rev.split('').reverse().join('');
+    var revString = mail.split('').reverse().join('');
+    var rev = revString.slice(4, 9);
+    var string = rev.split('').reverse().join('');
 
-  if (string === 'yahoo') {
-    res.send('Due to yahoo policy, yahoo doesnt allow third party email handlers to send email. So please enter an email address that is not yahoo.com');
-  } else {
+    if (string === 'yahoo') {
+        res.send('Due to yahoo policy, yahoo doesnt allow third party email handlers to send email. So please enter an email address that is not yahoo.com');
+    } else {
 
-    var email = new sendgrid.Email({
-      to: 'pradhanr03@gmail.com',
-      from: mail,
-      subject: 'Subject goes here',
-      text: message
-    });
-  }
-
-  sendgrid.send(email, function(err, json) {
-    if (err) {
-      return res.send('nope');
+        var email = new sendgrid.Email({
+            to: 'pradhanr03@gmail.com',
+            from: mail,
+            subject: 'Subject goes here',
+            text: message
+        });
     }
-    res.send('yay');
-  });
+
+    sendgrid.send(email, function(err, json) {
+        if (err) {
+            return res.send('nope');
+        }
+        res.send('yay');
+    });
 });
